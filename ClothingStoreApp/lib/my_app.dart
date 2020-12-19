256ccbb404933887f652c7f901f732bc04e9edc8
maciadalmau <47743853+maciadalmau@users.noreply.github.com>
2020-12-19 18:17:57 +0100

    Background

--- ClothingStoreApp/lib/my_app.dart
+++ ClothingStoreApp/lib/my_app.dart
@@ -9,44 +9,42 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'My App which needs login',
       home: Scaffold(
-        body: Stack(children: [
-          Container(
-            padding: EdgeInsets.all(30),
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  ButtonTheme(
+        body: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/background.jpg"),
+                  fit: BoxFit.cover)),
+          padding: EdgeInsets.all(35),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ButtonTheme(
                   height: 60,
-                  child: RaisedButton(
-                      onPressed: () {},
-                      color: Colors.white,
-                      child: Text(
-                        "HOMBRE",
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.black),
-                      ),
+                  child: FlatButton(
+                    onPressed: () {},
+                    color: Colors.white,
+                    child: Text(
+                      "MUJER",
+                      style: TextStyle(fontSize: 16, color: Colors.black),
                     ),
                   ),
-                  SizedBox(height: 20),
-                  ButtonTheme(
-                    height: 60.0,
-                    child: RaisedButton(
-                      onPressed: () {},
-                      color: Colors.white,
-                      child: Text(
-                        "MUJER",
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.black),
-                      ),
+                ),
+                SizedBox(height: 18),
+                ButtonTheme(
+                  height: 60.0,
+                  child: FlatButton(
+                    onPressed: () {},
+                    color: Colors.white,
+                    child: Text(
+                      "HOMBRE",
+                      style: TextStyle(fontSize: 16, color: Colors.black),
                     ),
                   ),
-                ]),
-          ),
-        ]),
+                ),
+              ]),
+        ),
       ),
     );
   }
